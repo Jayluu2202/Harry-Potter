367f3e988b74a95a597cf98bea8f28518525ce88
Jaymin <jaymin28panchal@gmail.com>
2026-08-08 16:23:28 +0530

    Initial Commit

---
+++ Harry Potter/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  Harry Potter
+//
+//  Created by Jayluu's Mac on 08/08/26.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ Harry Potter/Harry_PotterApp.swift
@@ -0,0 +1,17 @@
+//
+//  Harry_PotterApp.swift
+//  Harry Potter
+//
+//  Created by Jayluu's Mac on 08/08/26.
+//
+
+import SwiftUI
+
+@main
+struct Harry_PotterApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
